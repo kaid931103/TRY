7e649186cbfe4331f99bed0af2639bb4a6e75f3d
jingheng <henry860907@gmail.com>
2019-06-06 14:36:55 +0800

    build DAOShop & DBHelper

--- SwipeToDelete/app/src/main/java/com/example/swipetodelete/activity/AddDrinkItemActivity.java
+++ SwipeToDelete/app/src/main/java/com/example/swipetodelete/activity/AddDrinkItemActivity.java
@@ -1,25 +1,4 @@
 package com.example.swipetodelete.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
-
-import com.example.swipetodelete.R;
-
-public class AddDrinkItemActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_drink_item);
-
-        TextView tv1 = findViewById(R.id.text_view_1);
-        TextView tv2 = findViewById(R.id.text_view_2);
-
-        tv1.setText("來回答我問題");
-        tv2.setText("你最歡哪一家飲料店?");
-
-        tv1.setTextSize(40.0f);
-        tv2.setTextSize(20.0f);
-    }
+public class AddDrinkItemActivity {
 }
--- SwipeToDelete/app/src/main/java/com/example/swipetodelete/activity/MainActivity.java
+++ SwipeToDelete/app/src/main/java/com/example/swipetodelete/activity/MainActivity.java
@@ -7,21 +7,18 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.swipetodelete.R;
 import com.example.swipetodelete.adapter.RecyclerViewAdapter;
@@ -32,7 +29,6 @@ import com.example.swipetodelete.model.Shop;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -59,7 +55,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddDrinkItemActivity.class);
+                Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
                 startActivity(intent);
             }
         });
